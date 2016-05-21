f0af4c61e5cd9bd3cd930587c2e04fe1b8b5b9b8
Natalia Solomentseva <nsolomentseva.611@gmail.com>
2016-05-21 03:43:56 +0300

    Add files via upload

---
+++ javascript_context/javascript_context.js
@@ -0,0 +1,35 @@
+﻿function Calculator(initialValue) {
+
+    this.add = function (operand) {
+        this.result += operand;
+
+        return this;
+    };
+
+    this.multiply = function (operand) {
+        this.result *= operand;
+
+        return this;
+    };
+
+    this.subtract = function (operand) {
+        this.result -= operand;
+
+        return this;
+    };
+
+    this.divide = function (operand) {
+        this.result /= operand;
+
+        return this;
+    };
+
+    this.result = initialValue;
+}
+
+var calc = new Calculator(10);
+
+calc.add(3)
+    .subtract(1);
+
+console.log(calc.result);
